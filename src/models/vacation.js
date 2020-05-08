e6417e75a4f380659fd14ded47147fad85198619
Viggo Petersen <viggo.petersen@sallinggroup.com>
2020-05-08 14:27:04 +0200

    Add vacation class
    solves #25
    
    - add Vacation class
    - add test

---
+++ src/models/vacation.js
@@ -0,0 +1,30 @@
+'use strict'
+
+class Vacation {
+    constructor(startDate, endDate){
+        this._startDate = new Date(startDate);
+        this._endDate = new Date(endDate);
+    }
+
+    get startDate() {
+        return this._startDate;
+    }
+
+    get endDate() {
+        return this._endDate;
+    }
+
+    getStartWeek() {
+        return this._startDate.getWeek();
+    }
+
+    getEndWeek() {
+        return this._endDate.getWeek();
+    }
+
+    weekDifference() {
+        return this.getEndWeek() - this.getStartWeek();
+    }
+}
+
+module.exports = Vacation;
