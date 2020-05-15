b3c60710d9e14cf61d740f4af27923c7c45a0484
Kasper Leiszner <leisznern@gmail.com>
2020-05-15 19:07:37 +0200

    Removed dummy data

--- src/routes/project_details.js
+++ src/routes/project_details.js
@@ -3,23 +3,6 @@ const router = express.Router();
 
 const controller = require('../controllers/controller');
 
-// Dummy data
-const data = {
-    name: 'Bilka',
-    estimated_hours: '150',
-    workers: '6',
-    duration: '6 weeks',
-    red_weeks: '1',
-    developers: [
-        {name: 'Developer 1'},
-        {name: 'Developer 2'},
-        {name: 'Developer 3'},
-        {name: 'Developer 4'},
-        {name: 'Developer 5'},
-        {name: 'Developer 6'},
-    ]
-}
-
 router.get('/:name', async (req, res) => {
     let project = await controller.getProject(req.params.name);
 
